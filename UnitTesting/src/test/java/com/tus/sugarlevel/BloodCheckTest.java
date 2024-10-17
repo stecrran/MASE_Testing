148f647909db0677604a818ea2b4de078a828297
Stephen C <A00325351@student.tus.ie>
2024-10-17 22:50:33 +0100

    updating Blood Sugar test

--- UnitTesting/src/test/java/com/tus/sugarlevel/BloodCheckTest.java
+++ UnitTesting/src/test/java/com/tus/sugarlevel/BloodCheckTest.java
@@ -17,6 +17,37 @@ import org.junit.jupiter.params.provider.ValueSource;
 public class BloodCheckTest {
 	
 	BloodCheck bloodCheck = new BloodCheck();
+		
+	static Stream<Arguments> validBloodSugarLevels(){
+		return Stream.of(
+				Arguments.of(false, new double[] {0.0, 0.0, 0.0}),
+				Arguments.of(false, new double[] {2.5, 3.5, 4.5}),
+				Arguments.of(false, new double[] {2.5, 3.5, 4.5, 5.5, 6.5}),
+				Arguments.of(false, new double[] {2.5, 3.5, 4.5, 5.5, 6.5, 7.5}),
+				Arguments.of(true, new double[] {9.5, 10.5, 10.1}),
+				Arguments.of(true, new double[] {11, 11, 12, 12, 13}),
+				Arguments.of(true, new double[] {18, 19, 20, 19, 18, 17})
+				);	
+	}
+	
+	static Stream<Arguments> invalidBloodSugarLevels() {
+		 return Stream.of(
+				 Arguments.of( -0.2, new double[] { -0.2, 4, 4, 5, 6 } ),
+				 Arguments.of( -0.1, new double[] { 5, -0.1, 3, 4, 10} ),
+				 Arguments.of( 20.1, new double[] { 3, 3, 5, 4, 20.1 } ),
+				 Arguments.of( 20.2, new double[] { 5, 3, 3, 4, 20.2 } )
+		  );
+	} 
+	
+	
+	static Stream<Arguments> invalidNumBloodSugarLevels(){
+		return Stream.of(
+				Arguments.of(0, new double[] {}),
+				Arguments.of(1, new double[] {2.5}),
+				Arguments.of(2, new double[] {2.5, 3.5}),
+				Arguments.of(7, new double[] {2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5})
+				);	
+	}
 	
 	@ParameterizedTest
 	@ValueSource(doubles = {-0.2, -0.1, 20.1, 20.2})
@@ -33,32 +64,23 @@ public class BloodCheckTest {
 		assertEquals(result, bloodCheck.checkBloodSugarLevel(bloodSugarLevel));
 	}
 	
-	static Stream<Arguments> validBloodSugarLevels(){
-		return Stream.of(
-				Arguments.of(true, new double[] {2.5, 3.5, 4.5}),
-				Arguments.of(true, new double[] {2.5, 3.5, 4.5, 5.5, 6.5}),
-				Arguments.of(true, new double[] {2.5, 3.5, 4.5, 5.5, 6.5, 7.5})
-				);	
+	@ParameterizedTest
+	@MethodSource("invalidBloodSugarLevels")
+	void testInvalidBloodSugarLevels(double bloodSugarLevel, double[] sugarLevels) {
+		Throwable exception = assertThrows(IllegalArgumentException.class, () 
+				-> {bloodCheck.checkBloodSugarForDiabetesType2(sugarLevels);});
+		assertEquals("Sugar Level must be between 0.0 and 20.0: " + bloodSugarLevel + " not allowed.", exception.getMessage());
 	}
 	
 	@ParameterizedTest
 	@MethodSource("validBloodSugarLevels")
 	void testAverageForDiabetesType2(boolean result, double[] sugarLevels) {
-		assertFalse(bloodCheck.checkBloodSugarForDiabetesType2(sugarLevels));
-	}
-	
-	static Stream<Arguments> invalidBloodSugarLevels(){
-		return Stream.of(
-				Arguments.of(0, new double[] {}),
-				Arguments.of(1, new double[] {2.5}),
-				Arguments.of(2, new double[] {2.5, 3.5}),
-				Arguments.of(7, new double[] {2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5})
-				);	
+		assertEquals(result, bloodCheck.checkBloodSugarForDiabetesType2(sugarLevels));
 	}
 	
 	
 	@ParameterizedTest(name = "Sugar Level must be between 0.0 and 20.0: {0} not allowed.")
-	@MethodSource("invalidBloodSugarLevels")
+	@MethodSource("invalidNumBloodSugarLevels")
 	void testMultipleSafeLevelsInvalidLength(int num, double[] sugarLevels) {
 		Throwable exception = assertThrows(IllegalArgumentException.class, ()
 				-> {bloodCheck.checkBloodSugarForDiabetesType2(sugarLevels);});
